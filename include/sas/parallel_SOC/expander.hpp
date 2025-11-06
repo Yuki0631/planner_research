e5c42ce3d81df212f5ffd7f6faac724fc409499c
Yuki Suzuki <suzukisuzukisuzuki1228@gmail.com>
2025-11-06 15:01:43 +0900

    implement inplaced expander function

--- include/sas/parallel_SOC/expander.hpp
+++ include/sas/parallel_SOC/expander.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <functional>
 #include "sas/sas_reader.hpp"
 #include "sas/parallel_SOC/node.hpp"
 
@@ -17,6 +18,9 @@ struct Generated {
 class Expander {
 public:
     static void apply(const sas::Task& T, const sas::State& s, std::vector<Generated>& out);
+    
+    // インプレース化した関数 
+    static void for_each_inplace(const sas::Task& T, sas::State& s, const std::function<void(uint32_t /*op_id*/, int /*add_cost*/, const sas::State& /*succ*/)> &cb); // 引数にコールバック関数を入れる
 };
 
 // インプレース化などは後で作成
--- src/sas/parallel_SOC/expander.cpp
+++ src/sas/parallel_SOC/expander.cpp
@@ -1,5 +1,6 @@
 #include "sas/parallel_SOC/expander.hpp"
 #include <cassert>
+#include <utility>
 
 namespace planner {
  namespace sas {
@@ -65,7 +66,68 @@ void Expander::apply(const sas::Task& T, const sas::State& s, std::vector<Genera
     }
 }
 
-// インプレース関数などは後で実施する
+// インプレース関数の実装
+namespace {
+    // 変更した要素の情報を補完するための struct
+    struct DiffEntry{
+        uint32_t v; // 変更した変数の id
+        int old_val; // 元の domain-value
+    };
+
+    // 効果を適用する関数
+    inline void apply_effects(const Operator& op, State& s, std::vector<DiffEntry>& diff) {
+        diff.clear(); // 変更情報を積んだベクタをクリアする
+        diff.reserve(op.pre_posts.size()); // 効果の数だけ、ベクタの容量を確保する
+
+        for (auto& pp: op.pre_posts) {
+            uint32_t v = std::get<1>(pp); // 変更する変数 ID
+            int new_val = std::get<3>(pp);
+            int old_val = s[v];
+            if (old_val != new_val) {
+                diff.push_back({v, old_val});
+                s[v] = new_val;
+            }
+        }
+    }
+
+    // 効果の適用を戻す関数
+    inline void revert_effects(State& s, const std::vector<DiffEntry>& diff) {
+        for (const auto& d: diff) {
+            s[d.v] = d.old_val; // 変更した変数の domain-value を元の値に戻す
+        }
+    }
+} // namespace (anonymous)
+
+void Expander::for_each_inplace(const sas::Task& T, sas::State& s, const std::function<void(uint32_t, int, const sas::State&)>& cb) {
+    std::vector<DiffEntry> diff; // 変更情報を積んだベクタ
+
+    for (uint32_t oi=0; oi < T.ops.size(); ++oi) {
+        const auto& op = T.ops[oi];
+        if (!check_prevail(op, s)) { // prevail 条件を満たしているか確認する
+            continue;
+        }
+        if (!check_preconds(op, s)) { // pre_post の条件が一致するか確かめる
+            continue;
+        }
+
+        // 差分適用
+        apply_effects(op, s, diff);
+
+        // 必要なら、ここで mutex チェックを行う
+        // if (violates_mutex(T, s)) {
+        //     revert_effects(s, diff);
+        //     continue;
+        // }
+
+        // コールバック関数 (後継ノードは、今の s の参照で与えられる)
+        cb(oi, op.cost, s);
+
+        // 元に戻す
+        revert_effects(s, diff);
+    }
+
+}
+
 
 } // namespace parallel_SOC
 } // namespace sas
